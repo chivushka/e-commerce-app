66dab112b23bc1a9d5314400e1b536f5af068a1f
Ivanna <chivushka@gmail.com>
2023-08-24 08:55:33 +0300

    made part of home page, added components: navbar, announcement and slider

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://b2c-media.maxmara.com/sys-master/m0/MM/2023/1/4541083106/001/s3details/4541083106001-x-aloesj2_thumbnail.jpg#thumbnail-x",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "FAFAFA",
+    },
+    {
+        id: 2,
+        img: "https://b2c-media.maxmara.com/sys-master/m0/MM/2023/2/1016143306/001/s3details/1016143306001-f-teddy_thumbnail.jpg#thumbnail",
+        title: "WINTER COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "B4B0AF",
+    },
+    {
+        id: 3,
+        img: "https://us.maxmara.com/mediaObject/maxmara/101801_ADV/101801_ADV/MAX-MARA---ADV-ICON-COATS-2022---101801-2/original/MAX+MARA+-+ADV+ICON+COATS+2022+-+101801+2.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "E1D8D3",
+    },
+];
+
