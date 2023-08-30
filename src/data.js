464feebad68e856589d94035e97a3b03fdd18ff5
Ivanna <chivushka@gmail.com>
2023-08-30 13:00:32 +0300

    Revert "created separate folder for frontend"
    This reverts commit 2addc158f12980b9bcac66073414464ed2136b0e.

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://b2c-media.maxmara.com/sys-master/m0/MM/2023/1/4541083106/001/s3details/4541083106001-x-aloesj2_thumbnail.jpg#thumbnail-x",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "FAFAFA",
+    },
+    {
+        id: 2,
+        img: "https://b2c-media.maxmara.com/sys-master/m0/MM/2023/2/1016143306/001/s3details/1016143306001-f-teddy_thumbnail.jpg#thumbnail",
+        title: "WINTER COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "B4B0AF",
+    },
+    {
+        id: 3,
+        img: "https://us.maxmara.com/mediaObject/maxmara/101801_ADV/101801_ADV/MAX-MARA---ADV-ICON-COATS-2022---101801-2/original/MAX+MARA+-+ADV+ICON+COATS+2022+-+101801+2.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "E1D8D3",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id:2,
+        img:"https://b2c-media.maxmara.com/sys-master/m0/MM/2023/2/5046073906/009/s3details/5046073906009-w-napoli.jpg#zoom",
+    },
+    {
+        id:3,
+        img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id:4,
+        img:"https://cdn.shopify.com/s/files/1/0373/1177/4858/files/ZOELIE-DARKTAN-1.png?v=1689771602&width=533",
+    },
+    {
+        id:5,
+        img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id:6,
+        img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id:7,
+        img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    },
+    {
+        id:8,
+        img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+]
